c42cc2ca9d7c66ff31c25a478b9fcc3c4c9c1706
Daniel Szemerey <account@szemerey.eu>
2022-09-21 12:37:57 +0200

    feat: Added workflow, protected branches and tests.

--- explore.py
+++ explore.py
@@ -1,19 +1,15 @@
 import pandas as pd
-from utils import to_token_list, write_tokens, print_examples
+from utils import to_token_list, write_dataframe, print_examples, read_all_files
 from constants import CONST
 
 
-df = pd.DataFrame([])
-for i in range(5):
-    df = pd.concat(
-        [df, pd.read_json(f"{CONST.data_root_path}/original/training_{i}.json")]
-    )
+df = read_all_files(f"{CONST.data_root_path}/original", name="training_")
 
 print_examples(df, 2)
 
 df["tokenized"] = df["code"].apply(lambda x: to_token_list(x))
 
-write_tokens(
+write_dataframe(
     df,
     f"{CONST.data_root_path}/processed",
     name="training_",
